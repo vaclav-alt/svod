0ae7d43d6596c64fd11db5ba4988c2eac84df75c
Vaclav Alt <vaclav.alt@seznam.cz>
2019-02-11 23:43:41 +0100

    fix missing mkn in data structure

--- download_svod.py
+++ download_svod.py
@@ -17,7 +17,7 @@ kraje = ["PHA", "STC", "JHC", "PLK", "KVK", "ULK", "LBK", "HKK", "PAK", "OLK", "
 mkn = ["C00", "C01", "C02", "C03", "C04", "C05", "C06", "C07", "C08", "C09", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20", "C21", "C22", "C23", "C24", "C25", "C26", "C30", "C31", "C32", "C33", "C34", "C37", "C38", "C39", "C40", "C41", "C43", "C44", "C45", "C46", "C47", "C48", "C49", "C50", "C51", "C52", "C53", "C54", "C55", "C56", "C57", "C58", "C60", "C61", "C62", "C63", "C64", "C65", "C66", "C67", "C68", "C69", "C70", "C71", "C72", "C73", "C74", "C75", "C76", "C77", "C78", "C79", "C80", "C81", "C82", "C83", "C84", "C85", "C86", "C88", "C90", "C91", "C92", "C93", "C94", "C95", "C96", "C97", "D03"]
 
 
-sql_query = '''insert into incmort (pohlavi, vek, stadium, region, t, n, m, rok, zije, umrti, inc, mort) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
+sql_query = '''insert into incmort (pohlavi, mkn, vek, stadium, region, t, n, m, rok, zije, umrti, inc, mort) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
 
 def vekDb(i):
     k = (i - 1)*5
@@ -51,24 +51,24 @@ def getUrlOpts(c):
     opts = {
         "sessid" : "slr1opn84pssncqr5hekcj6d87",
         "typ" : "incmor",
-        "diag" : "C53",
+        "diag" : c[1],
         "pohl" : c[0],
-        "kraj" : c[3],
-        "vek_od" : c[1],
-        "vek_do" : c[1],
+        "kraj" : c[4],
+        "vek_od" : c[2],
+        "vek_do" : c[2],
         "zobrazeni" : "table",
         "incidence" : "1",
         "mortalita" : "1",
         "mi" : "0",
-        "vypocet" : "w",
-        "obdobi_od" : c[7],
-        "obdobi_do" : c[7],
-        "stadium" : c[2],
-        "t" : c[4],
-        "n" : c[5],
-        "m" : c[6],
-        "zije" : c[8],
-        "umrti" : c[9],
+        "vypocet" : "a",
+        "obdobi_od" : c[8],
+        "obdobi_do" : c[8],
+        "stadium" : c[3],
+        "t" : c[5],
+        "n" : c[6],
+        "m" : c[7],
+        "zije" : c[9],
+        "umrti" : c[10],
         "lecba" : ""
     }
     return opts
@@ -97,11 +97,13 @@ def main():
         print("ANI HOVNO")
     print("incidence: %.2f\tmortalita: %.2f" % incmort)
 
-#for x in product(pohl, veky, stadia):
-#    combo = list(x)
-#    combo[1] = vekDb(combo[1])
-#    print(combo) 
-#    break
+# sql_query = '''insert into incmort (pohlavi, mkn, vek, stadium, region, t, n, m, rok, zije, umrti, inc, mort) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
+    for x in product(pohl, mkn, veky, stadia, kraje, tnm_t, tnm_n, tnm_m, roky, zije, umrti):
+        print(getUrl(getUrlOpts(x)))
+        combo = list(x)
+        combo[2] = vekDb(combo[2])
+        print(combo) 
+        break
 
 # db = sq.connect("svod.sqlite")
 # cursor  = db.cursor()
