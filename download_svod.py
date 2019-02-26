206e1777f6093f60e3ba20b3902d2d508c31b913
Vaclav Alt <vaclav.alt@seznam.cz>
2019-02-26 16:36:29 +0100

    opt-mgr wip

--- download_svod.py
+++ download_svod.py
@@ -6,29 +6,6 @@ import configparser, os, time
 from itertools import product
 from progress.bar import IncrementalBar
 
-pohl = [ "m", "z"]
-#roky = range(1977, 1997)
-roky = range(1997, 2017)
-veky = range(1, 19)
-zije = [""]
-umrti = [""]
-# zije = [0, 1]
-# umrti = [0, 1]
-tnm_t = [""]
-tnm_n = [""]
-tnm_m = [""]
-#tnm_t = ["0", "1", "2", "3", "4", "A", "S", "X"]
-#tnm_n = ["0", "1", "2", "3", "4", "X"]
-#tnm_m = ["0", "1", "X"]
-stadia = ["X", "1", "2", "3", "4"]
-#kraje = ["PHA", "STC", "JHC", "PLK", "KVK", "ULK", "LBK", "HKK", "PAK", "OLK", "MSK", "JHM", "ZLK", "VYS"]
-kraje = [""]
-#mkn = ["C00", "C01", "C02", "C03", "C04", "C05", "C06", "C07", "C08", "C09", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20", "C21", "C22", "C23", "C24", "C25", "C26", "C30", "C31", "C32", "C33", "C34", "C37", "C38", "C39", "C40", "C41", "C43", "C44", "C45", "C46", "C47", "C48", "C49", "C50", "C51", "C52", "C53", "C54", "C55", "C56", "C57", "C58", "C60", "C61", "C62", "C63", "C64", "C65", "C66", "C67", "C68", "C69", "C70", "C71", "C72", "C73", "C74", "C75", "C76", "C77", "C78", "C79", "C80", "C81", "C82", "C83", "C84", "C85", "C86", "C88", "C90", "C91", "C92", "C93", "C94", "C95", "C96", "C97", "D03"]
-mkn = ["C01"]
-
-
-sql_query = '''insert into incmort (pohlavi, mkn, vek, stadium, region, t, n, m, rok, zije, umrti, inc, mort) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
-
 def vekDb(i):
     k = (i - 1)*5
     return "v_%d" % k
@@ -58,6 +35,8 @@ options = {
 }
 
 class SvodMaster:
+    # TODO(vaclav): SvodMaster should iterate over URLs, which should be handled only by optmgr
+
     def __init__(self, filename):
         self.cfg = configparser.ConfigParser()
         self.cfg.read(filename)
@@ -111,6 +90,7 @@ class SvodMaster:
         opts.extend(list(incmort))
         self.c.execute(sql_query, opts)
         
+    # TODO(vaclav): move to opt-mgr
     def _getUrlOpts(self, c):
         opts = {
             "sessid" : "slr1opn84pssncqr5hekcj6d87",
@@ -137,6 +117,7 @@ class SvodMaster:
         }
         return opts
 
+    # TODO(vaclav): move to opt-mgr
     def _getUrl(self, options):
         url = "http://www.svod.cz/graph/?"
 
@@ -157,8 +138,7 @@ class SvodMaster:
         incmort = self._processUrl(url)
         print(incmort)
 
-
-
+    # TODO(vaclav): move to opt-mgr
     def getTaskCount(self):
         return len(pohl) * len(mkn) * len(veky) * len(stadia) * len(kraje) * len(tnm_t) * len(tnm_n) * len(tnm_m) * len(roky) * len(zije) * len(umrti)
 
--- opt-mgr.py
+++ opt-mgr.py
@@ -1,18 +1,103 @@
 #!/usr/bin/python3
 
 import configparser 
+import re
+
+'''
+pohl = [ "m", "z"]
+veky = range(1, 19)
+zije = [0, 1]
+umrti = [0, 1]
+tnm_t = ["0", "1", "2", "3", "4", "A", "S", "X"]
+tnm_n = ["0", "1", "2", "3", "4", "X"]
+tnm_m = ["0", "1", "X"]
+stadia = ["X", "1", "2", "3", "4"]
+kraje = ["PHA", "STC", "JHC", "PLK", "KVK", "ULK", "LBK", "HKK", "PAK", "OLK", "MSK", "JHM", "ZLK", "VYS"]
+mkn = ["C01"]
+'''
 
 class OptMaster:
     def __init__(self, filename="opts.ini"):
         self.cfg = configparser.ConfigParser()
         self.cfg.read(filename)
 
-    def getMkn(self):
-        print(self.cfg.options("mkn"))
+        # obecne
+        self.pohlavi = []
+        self.zije = []
+        self.umrti = []
+        self.kraje = []
+        self.stadium = []
+        self.mkn = []
+
+        # roky
+        self.yearStart = 0
+        self.yearEnd = 0
+
+        # vek
+        self.vek = []
+
+        # tnm
+        self.t = []
+        self.n = []
+        self.m = []
+
+    def load(self):
+        self.pohlavi.extend(self.cfg["obecne"]["pohlavi"].split(','))
+        self.zije.extend(self.cfg["obecne"]["zije"].split(','))
+        self.umrti.extend(self.cfg["obecne"]["umrti"].split(','))
+        self.kraje.extend(self.cfg["obecne"]["kraje"].split(','))
+        self.stadium.extend(self.cfg["obecne"]["stadium"].split(','))
+
+        self.yearStart = int(self.cfg["roky"]["start"])
+        self.yearEnd = int(self.cfg["roky"]["end"])
+
+        a = int(self.cfg["vek"]["start"])
+        b = int(self.cfg["vek"]["end"])
+        self.vek = list(range(a, b+1))
+
+        self.t.extend(self.cfg["tnm"]["t"].split(','))
+        self.n.extend(self.cfg["tnm"]["n"].split(','))
+        self.m.extend(self.cfg["tnm"]["m"].split(','))
+
+        self.mkn.extend(self._parseMkn(self.cfg["mkn"]["C"], 'C'))
+        self.mkn.extend(self._parseMkn(self.cfg["mkn"]["D"], 'D'))
+        self.mkn.append(self.cfg["mkn"]["special"])
+
+    def _parseRange(self, s):
+        mkn = []
+        for d in s.split(','):
+            if re.search("\d+[-]{1}\d+", d):
+                a = int(re.search("^\d+", d).group(0))
+                b = int(re.search("\d+$", d).group(0))
+                for x in range(a,b+1):
+                    mkn.append(x)
+            else:
+                mkn.append(d)
+        return mkn
+
+    def _parseMkn(self, s, pref):
+        mkn = []
+        for x in self._parseRange(s):
+            mkn.append(pref + str(x))
+        return mkn
+
 
 def main():
-    prd = OptMaster()
-    prd.getMkn()
+    opt = OptMaster()
+    opt.load()
+    print(opt.pohlavi)
+    print(opt.zije)
+    print(opt.umrti)
+    print(opt.kraje)
+    print(opt.stadium)
+    print(opt.yearStart)
+    print(opt.yearEnd)
+    print(opt.vek)
+    print(opt.t)
+    print(opt.n)
+    print(opt.m)
+    print(opt.mkn)
+
 
 if __name__ == "__main__":
     main()
